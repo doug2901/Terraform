7b604aa1cbc8f672ea5725cad59bdc6610d85819
Doug <douglas.noronha91@gmail.com>
2022-01-01 13:31:30 -0300

    creating a DNS record

--- study_project/dns.tf
+++ study_project/dns.tf
@@ -4,11 +4,11 @@ resource "azurerm_dns_zone" "dnfcorp" {
   tags = {Study_Project = "DNS"}
 }
 
-/*
+
 resource "azurerm_dns_a_record" "test" {
   name                = "test"
   zone_name           = azurerm_dns_zone.dnfcorp.name
   resource_group_name = azurerm_resource_group.RG-ADM.name
   ttl                 = 3600
   records             = ["10.0.180.17"]
-}*/
+}
