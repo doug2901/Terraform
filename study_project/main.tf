60af55ad23d691b72979e6f13c8356439500b0e8
douglas <douglas.noronha91@gmail.com>
2024-05-18 16:04:36 -0300

    add OCI resources

--- study_project/main.tf
+++ study_project/main.tf
@@ -1,18 +1,32 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.97.1"
+    }
+    oci = {
+      source  = "hashicorp/oci"
+      version = "5.39.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "RG-Storage"
     storage_account_name = "dnfcorpterraform"
     container_name       = "tfstate"
     key                  = "study.terraform.tfstate"
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.97.1"
-    }
-  }
 }
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+# Configure the Oracle Cloud Infrastructure Provider
+provider "oci" {
+  region           = var.oci_region
+  tenancy_ocid     = var.oci_tenancy_ocid
+  user_ocid        = var.oci_user_ocid
+  fingerprint      = var.oci_fingerprint
+  private_key_path = var.oci_private_key_path
 }
---
+++ study_project/oci-instances.tf
@@ -0,0 +1,45 @@
+resource "oci_core_instance" "mimir" {
+
+	availability_domain = "cWAI:US-ASHBURN-AD-1"
+	compartment_id = oci_identity_compartment.valhalla.id
+	create_vnic_details {
+		assign_public_ip = "true"
+		subnet_id = oci_core_subnet.asgard.id
+ 	}
+	display_name = "mimir"
+	metadata = {
+		"ssh_authorized_keys" = var.oci_public_ssh_key
+	}
+	shape = "VM.Standard.E2.1.Micro"
+	source_details {
+		source_id = var.oci_rocky_image
+		source_type = "image"
+	}
+}
+
+output "mimir_public_ip" {
+  value = oci_core_instance.mimir.public_ip
+}
+
+resource "oci_core_instance" "freya" {
+
+	availability_domain = "cWAI:US-ASHBURN-AD-1"
+	compartment_id = oci_identity_compartment.valhalla.id
+	create_vnic_details {
+		assign_public_ip = "true"
+		subnet_id = oci_core_subnet.asgard.id
+ 	}
+	display_name = "freya"
+	metadata = {
+		"ssh_authorized_keys" = var.oci_public_ssh_key
+	}
+	shape = "VM.Standard.E2.1.Micro"
+	source_details {
+		source_id = var.oci_ubuntu_image
+		source_type = "image"
+	}
+}
+
+output "freya_public_ip" {
+  value = oci_core_instance.freya.public_ip
+}
