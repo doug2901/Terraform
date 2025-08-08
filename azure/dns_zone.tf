f5ab5410dbb6cba35a48c43270cdff8d42a9d43c
Douglas <douglas.noronha91@gmail.com>
2025-08-08 20:49:24 -0300

    ajustes

--- azure/dns_zone.tf
+++ azure/dns_zone.tf
@@ -4,10 +4,10 @@ resource "azurerm_dns_zone" "dnfcorp" {
   tags = {Study_Project = "DNS"}
 }
 
-resource "azurerm_dns_a_record" "mimir" {
-  name                = "mimir"
-  zone_name           = azurerm_dns_zone.dnfcorp.name
-  resource_group_name = azurerm_resource_group.RG-ADM.name
-  ttl                 = 3600
-  records             = ["150.230.171.85"]
-}
+#resource "azurerm_dns_a_record" "mimir" {
+#  name                = "mimir"
+#  zone_name           = azurerm_dns_zone.dnfcorp.name
+#  resource_group_name = azurerm_resource_group.RG-ADM.name
+#  ttl                 = 3600
+#  records             = ["150.230.171.85"]
+#}
